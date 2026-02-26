1ccac59b70bf7f8a8787f704252f6774c4afd212
Mayara Santos <51439466+may-santos@users.noreply.github.com>
2026-02-26 00:42:06 +0000

    vetorparimpar

--- VetorParImpar.py
+++ VetorParImpar.py
@@ -1,10 +1,18 @@
-valor = int(input())
-vetor = list(map(int, input().split()))
+saidas = []
 
-for i in range(valor):
-    if vetor[i] % 2 == 0:
-        vetor[i] = 0
-    else:
-        vetor[i] = 1
+while True:
+    valor = int(input())
+    if valor == 0:
+        break
 
-print(*vetor)
+    vetor = list(map(int, input().split()))
+    for i in range(valor):
+        if vetor[i] % 2 == 0:
+            vetor[i] = 0
+        else:
+            vetor[i] = 1
+
+    saidas.append(" ".join(map(str, vetor)))
+
+if saidas:
+    print("\n".join(saidas))
