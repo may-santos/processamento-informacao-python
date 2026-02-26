f2d82e809983e6fd2357d5223a3bf61f799483d9
Mayara Santos <51439466+may-santos@users.noreply.github.com>
2026-02-26 00:46:14 +0000

    Aprovado refactor.

--- Aprovado.py
+++ Aprovado.py
@@ -2,7 +2,7 @@ media_falta = input().split()
 media = float(media_falta[0])
 faltas = int(media_falta[1])
 
-if media >= 6 and faltas < 30:
+if media >= 6.0 and faltas <= 30:
     print("Aprovado!")
 else:
     print()
